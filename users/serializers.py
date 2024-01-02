7f036d9cdb3e66425aa608c2c8be27a7fe6b57bb
shulaev ivan <mail@ntdservice.ru>
2024-01-02 22:54:18 +0300

    add some data

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from users.models import DndUser
+from .models import UserCharacter
 
 
-class UserCharacterSerializer(ModelSerializer):
-    pass
+class UserCharacterSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = UserCharacter
+        exclude = [
+            'id',
+            'character_description',
+            'character_avatar',
+            'character_level',
+            'character_race',
+            'character_class',
+            'character_subclass',
+            'character_background',
+            'dnd_user',
+        ]
+
+    def validate_character_name(self, value):
+        if value == '' or len(value) < 3:
+            raise serializers.ValidationError("Email cannot be empty.", code="invalid")
+        
+        return super().validate(value)
--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import UserCharacterSerializer
+
 class UserCharacterView(APIView):
 
     def get(self, request, user_id):
@@ -15,6 +17,11 @@ class UserCharacterView(APIView):
             return Response('test', status=status.HTTP_200_OK)
         
     def post(self, request, user_id):
-        print(request.data)
+        character_data = {
+            'character_name': request.data.get('charName')
+        }
+        print(len(character_data['character_name']))
+        char_serializer = UserCharacterSerializer(data=character_data)
+        print(char_serializer.is_valid(raise_exception=True))
 
         return Response(request.data, status=status.HTTP_201_CREATED)

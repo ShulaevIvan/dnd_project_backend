0097d5092b9e6e8f01299fd9c834fa9d008e5f7c
shulaev ivan <mail@ntdservice.ru>
2024-01-03 23:23:01 +0300

    add some data

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from pprint import pprint
 
 from .serializers import UserCharacterSerializer
 
@@ -14,14 +15,33 @@ class UserCharacterView(APIView):
         query = get_object_or_404(DndUser, id=user_id)
 
         if query.email:
-            return Response('test', status=status.HTTP_200_OK)
+            user_characters = query.character.all().values()
+            
+            return Response(user_characters, status=status.HTTP_200_OK)
         
     def post(self, request, user_id):
         character_data = {
-            'character_name': request.data.get('charName')
+            'character_name': request.data.get('charName'),
+            'character_description': request.data.get('charDescription'),
+            'character_avatar': 'test',
+            'character_level': request.data.get('charLvl'),
+            'character_race' : request.data.get('charRace'),
+            'character_class':  request.data.get('charClass'),
+            'character_subclass': 'test false',
+            'character_background': request.data.get('charBackground')
         }
-        print(len(character_data['character_name']))
-        char_serializer = UserCharacterSerializer(data=character_data)
-        print(char_serializer.is_valid(raise_exception=True))
+        query = UserCharacter.objects.get_or_create(
+            dnd_user_id=user_id,
+            character_name=character_data['character_name'],
+            character_description=character_data['character_description'],
+            character_avatar=character_data['character_avatar'],
+            character_level=character_data['character_level'],
+            character_race=character_data['character_race'],
+            character_class=character_data['character_class'],
+            character_subclass=character_data['character_subclass'],
+            character_background=character_data['character_background'],
+        )
+        # char_serializer = UserCharacterSerializer(data=character_data, many=True)
+        # print(char_serializer.is_valid(raise_exception=True))
 
         return Response(request.data, status=status.HTTP_201_CREATED)
